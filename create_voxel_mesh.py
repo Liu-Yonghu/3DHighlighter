54cbaf534ceb48151d6fe3213c4fb1dd7f96a48d
Liu Yonghu <liuyonghu0110@126.com>
2025-03-12 18:12:43 +0100

    correct voxel mesh generation

--- create_voxel_mesh.py
+++ create_voxel_mesh.py
@@ -1,8 +1,11 @@
 import pickle
 import os
 import math
+
+import numpy as np
 import torch
 import trimesh
+from utils import device
 from pathlib import Path
 
 from converter import generate_clip_sentences, point_to_voxel, voxel_to_meshs
@@ -10,40 +13,49 @@ import random
 from converter import AffordNetDataset
 
 
-def create_voxel_mesh(args):
+def create_voxel_from_mesh(args):
     data_dir = 'data_extension'
     data_name = 'full_shape_val_data.pkl'
 
     affordnet = AffordNetDataset(data_dir, data_name)
     data = affordnet.load_data()
+    if not data:
+        raise ValueError("Loaded data is empty. Please check dataset path and contents.")
+
     print(data[0].keys())
 
     save_path = (os.path.join(data_dir, 'data_from_voxel'))
     Path(save_path).mkdir(parents=True, exist_ok=True)
 
     random.seed(args.seed)
-    rand_index = random.randint(0, len(data)-1)
+    rand_index = random.randint(0, len(data) - 1)
     single_object = data[rand_index]
+
+    if 'semantic_class' not in single_object or 'labels' not in single_object or 'coordinates' not in single_object:
+        raise KeyError("Missing required keys in data entry.")
+
     clip_texts = generate_clip_sentences(single_object['semantic_class'], single_object['labels'])
 
-    single_object['coordinates'] = torch.tensor(single_object['coordinates']).unsqueeze(0)
-    print(single_object['coordinates'].shape)
-    voxel = point_to_voxel(single_object['coordinates'])
-    voxel = voxel.to("cuda")
+    coordinates = torch.tensor(single_object['coordinates']).unsqueeze(0)
+    print("Coordinates Shape:", coordinates.shape)
+    print("Coordinates Device:", coordinates.device)
+
+    voxel = point_to_voxel(coordinates).to(device)
     vertices, faces = voxel_to_meshs(voxel)
 
-    mesh = trimesh.Trimesh(vertices, faces)
-    mesh.export(f"{single_object['semantic_class']}.obj", save_path)
+    vertices = vertices[0].squeeze(0)  # shape to (64, 3)
+    vertices_ = vertices.cpu().numpy()
+    faces = faces[0].squeeze(0)  # shape to  (64, 3)
+    faces_ = faces.cpu().numpy()
+
+    mesh = trimesh.Trimesh(vertices=vertices_, faces=faces_)
+
+    obj_file_path = os.path.join(save_path, f"{single_object['semantic_class']}.obj")
+    mesh.export(obj_file_path)
+    print(f"Mesh saved at: {obj_file_path}")
 
     args.object = single_object['semantic_class']
     args.classes = single_object['labels']
     args.prompt = clip_texts
 
-    return vertices, faces, args
-
-    #obj.export_obj(save_path, vertices, faces)
-    #print(f"OBJ saved under: {save_path}")
-
-
-
-
+    return args
